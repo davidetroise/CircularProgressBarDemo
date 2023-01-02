fafe4905bb9ec10b4a473ec6755d57a2caf47ce3
Davide Troise <davide@troise.net>
2023-01-02 01:33:53 +0100

    Complete code

--- CircularProgressBarDemo/ContentView.swift
+++ CircularProgressBarDemo/ContentView.swift
@@ -8,13 +8,39 @@
 
 import SwiftUI
 
+struct CircularProgressBar: View {
+    let value: Double
+    let total: Double
+    
+    var body: some View {
+        ZStack {
+            Circle()
+                .stroke(lineWidth: 30)
+                .opacity(0.3)
+            
+            Circle()
+                .trim(from: 0.0, to: CGFloat(min(value/total, 1.0)))
+                .stroke(style: StrokeStyle(lineWidth: 30, lineCap: .round, lineJoin: .round))
+                .rotationEffect(Angle(degrees: 270))
+                .animation(.easeOut, value: value)
+        }
+        .foregroundColor(.blue)
+    }
+}
+
 struct ContentView: View {
+    @State private var progress: Double = 0.0
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            CircularProgressBar(value: progress, total: 100)
+                .padding()
+            Button {
+                self.progress += 10
+            } label: {
+                Text("Increment Progress")
+            }
+
         }
         .padding()
     }
